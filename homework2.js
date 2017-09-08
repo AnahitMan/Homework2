c7199eebc5cb9fa026c08092a9c1ead177a79489
Anahit20 <anahit.manukyan.2015@mail.ru>
2017-09-08 16:05:31 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,22 @@
+//variables
+const num1=56-24;
+const num2=num1/(num1*2);
+ console.log(num2);
+
+ const dressprice=250;
+ const shoesprice=464;
+ console.log( dressprice+shoesprice);
+ 
+ alert("hello");
+ //console
+console.log(true);
+"Study JS to create your carrer by your hands!"
+//arithmetic operators 
+console.log((4+15)/5);
+console.log(num2/(num1*13-24));
+console.log('I feel it comming'+ 24/6 );
+console.log(12 === 12);
+console.log(14-4 <= 18/2);
+//boolean operators
+console.log(true && !!! false);
+console.log( !(false || true) && !!true );
